39b22f9c4e9d812755575413154e990d940514ed
ayamit92 <ayamit92@gmail.com>
2018-10-06 15:12:54 +0530

    deleting comments

--- app/src/main/java/com/ayamit92/kbc/ScoreActivity.java
+++ app/src/main/java/com/ayamit92/kbc/ScoreActivity.java
@@ -39,8 +39,5 @@ public class ScoreActivity extends AppCompatActivity {
         String result=correct+"/"+total;
 
         score.setText(result);
-
-        //checking git
-        //commit3
     }
 }
